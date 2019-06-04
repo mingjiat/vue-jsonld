9b048500dd196817e4ce4a56044c6329858fc978
areschen <ariesjia00@hotmail.com>
2019-06-04 18:40:47 +0800

    feat: add get method to get current meta info

--- src/jsonld.ts
+++ src/jsonld.ts
@@ -1,13 +1,17 @@
 import { buildHtml } from "./server";
 
-const _text = function() {
-  return buildHtml(this.$root._jsonld.data)
-}
-
 export default function(options : Option) {
   return function $meta() {
+
+    const get = () => {
+      return this.$root._jsonld.data
+    }
+
     return {
-      text: _text.bind(this),
+      text() {
+        return buildHtml(get())
+      },
+      get,
     }
   }
 }
